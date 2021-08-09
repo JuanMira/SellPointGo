78e7acb0314f717dcbbb5e376cfd2d0a2f4a4029
JuanMira <juanmira20@hotmail.com>
2021-08-09 04:33:23 -0500

    Add bcrypt, godotenv library

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ set -xe
 go get github.com/gin-gonic/gin
 go get gopkg.in/go-playground/validator.v9
 go get -u gorm.io/gorm
+go get golang.org/x/crypto/bcrypt
+go get github.com/joho/godotenv
 
 #build command
 go build -o bin/application main.go
