ebb1ae557f4a5e69b32205e8177b8fa532076fa5
JuanMira <juanmira20@hotmail.com>
2021-08-14 22:17:10 -0500

    Adding products routes

--- routes/routes.go
+++ routes/routes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	product_controller "gingonic/controllers/Product"
 	"gingonic/controllers/auth"
 	"gingonic/middleware"
 
@@ -11,12 +12,15 @@ var Router *gin.Engine
 
 func InitializeRoutes() {
 
+	//setup global middleware		
+
 	authRoute := Router.Group("/api/auth")
 	{
 		authRoute.POST("/login", auth.Login)
 		authRoute.POST("/register", auth.Register_c)
 	}
 
+	//example
 	storeRoute := Router.Group("/api/store")
 	{
 		storeRoute.GET("/", middleware.VerifyToken(),func(c *gin.Context) {
@@ -24,5 +28,16 @@ func InitializeRoutes() {
 				"debug":"test",
 			})
 		})
+	}
+
+	productRoute := Router.Group("/api/product")
+	{
+		productRoute.POST("/",
+			middleware.VerifyToken(),
+			middleware.VerifyAdmin(),
+			product_controller.CreateProduct_c,
+		)
+		productRoute.GET("/all",product_controller.StoreList_c)		
+		productRoute.GET("/")		
 	}
 }
