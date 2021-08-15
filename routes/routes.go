ab0780dc49081fff4f006a291480432d94ee32a8
JuanMira <juanmira20@hotmail.com>
2021-08-15 16:25:37 -0500

    Refactor routes to better scaling app

--- routes/routes.go
+++ routes/routes.go
@@ -1,10 +1,6 @@
 package routes
 
 import (
-	product_controller "gingonic/controllers/Product"
-	"gingonic/controllers/auth"
-	"gingonic/middleware"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,32 +8,8 @@ var Router *gin.Engine
 
 func InitializeRoutes() {
 
-	//setup global middleware		
-
-	authRoute := Router.Group("/api/auth")
-	{
-		authRoute.POST("/login", auth.Login)
-		authRoute.POST("/register", auth.Register_c)
-	}
-
-	//example
-	storeRoute := Router.Group("/api/store")
-	{
-		storeRoute.GET("/", middleware.VerifyToken(),func(c *gin.Context) {
-			c.JSON(200,gin.H{
-				"debug":"test",
-			})
-		})
-	}
-
-	productRoute := Router.Group("/api/product")
-	{
-		productRoute.POST("/",
-			middleware.VerifyToken(),
-			middleware.VerifyAdmin(),
-			product_controller.CreateProduct_c,
-		)
-		productRoute.GET("/all",product_controller.StoreList_c)		
-		productRoute.GET("/")		
-	}
+	//setup global middleware
+	authRoutes()
+	productRoutes()
+	categoriesRoutes()
 }
