f344940f56e349f9b7d47d2acc2574648dfec947
JuanMira <juanmira20@hotmail.com>
2021-08-15 16:24:08 -0500

    Refactor to category model add column status to true and false

--- core/Category.go
+++ core/Category.go
@@ -1,8 +1,15 @@
 package core
 
 type Category struct {
-	ID          uint64    `gorm:"primaryKey" json:"categoryId"`
-	Name        string    `gorm:"unique;not null" json:"categoryName"`
-	Description string    `json:"categoryDescription"`
-	Products    *Products `gorm:"many2many:product_categories"`
+	ID          uint64 `gorm:"primaryKey" json:"categoryId"`
+	Name        string `gorm:"unique;not null" json:"categoryName"`
+	Description string `json:"categoryDescription"`
+	Status      bool   `gorm:"default:true"`
+}
+
+type Category_Response struct {
+	ID          uint64 `json:"categoryId"`
+	Name        string `json:"categoryName"`
+	Description string `json:"categoryDescription"`
+	Status      bool   `json:"categoryStatus,omitempty"`
 }
