6cb961523c5849b8ba7fdaf6f0fdce7fc09c2e83
JuanMira <juanmira20@hotmail.com>
2021-08-14 22:09:13 -0500

    Add env files laod and initialize sesison in aws s3

--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"gingonic/migrations"
 	"gingonic/routes"
+	"gingonic/services"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	routes.Router = gin.Default()
+	err := godotenv.Load()
+
+	if err != nil {
+		fmt.Println("file .env is missing")
+		return
+	} 
+
+	sess := services.ConnectAWS()
+
+	routes.Router.Use(func(c *gin.Context){
+		c.Set("sess",sess)
+		c.Next()
+	})
+
 	port := os.Getenv("PORT")
 
 	migrations.Migrate()
